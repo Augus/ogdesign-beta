bd8139ca23cb997edd57376bbab899cd40e8c7b6
Augus Chen <augus@ogdesign.tw>
2016-08-10 22:28:00 +0800

    [新增] 常見問題後台管理

---
+++ models/Question.js
@@ -0,0 +1,22 @@
+var keystone = require(__base + 'keystone_custom');
+var Types = keystone.Field.Types;
+
+/**
+ * Question Model
+ * ==========
+ */
+
+var Question = new keystone.List('Question', {
+	map: { name: 'title' },
+	label: '常見問題',
+	sortable: true,
+});
+
+Question.add({
+	title: { type: String, required: true, label: '問題名稱', initial: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' }, label: '發佈日期', initial: true },
+	content: {type: Types.Html, wysiwyg: true, height: 300, label: '內文', initial: true},
+});
+
+Question.defaultColumns = 'title, content, publishedDate|20%';
+Question.register();
--- routes/views/qa.js
+++ routes/views/qa.js
@@ -5,6 +5,19 @@ exports = module.exports = function(req, res) {
     var view = new keystone.View(req, res);
     var locals = res.locals;
     locals.section = 'qa';
-    locals.data = {};
+    locals.data = {
+    	questions : []
+    };
+
+    view.on('init', function(next) {
+		var q = keystone.list('Question').model.find()
+			.sort('sortOrder')
+		.exec(function(err, questions) {
+			locals.data.questions = questions;
+			next(err);
+		});
+		
+	});
+
     view.render('qa');
 };
